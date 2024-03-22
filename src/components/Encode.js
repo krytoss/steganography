c8edd0051c065acb8d0623aa037c35d23179dadc
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-22 01:24:31 +0100

    changed the App.css + added Button component

--- src/components/Encode.js
+++ src/components/Encode.js
@@ -1,15 +1,22 @@
 import Input from "./elements/Input"
 import Textarea from "./elements/Textarea"
+import Button from "./elements/Button"
 
 const Encode = ({ imageData, loadImage }) => {
 
     return (
         <div id='encode'>
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-3 gap-4">
                 <div>
                     { imageData && <img alt='Uploaded image' src={ imageData } />}
                     { imageData && <Textarea />}
                     <Input loadImage={ loadImage } type='file' />
+                </div>
+                <div>
+                    { imageData && <Button value='Encode the message' />}
+                </div>
+                <div>
+
                 </div>
             </div>
         </div>
