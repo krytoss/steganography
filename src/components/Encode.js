9332085632160376e55047ce4e01ef7966a6ff16
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-22 01:36:13 +0100

    changed textarea to controlled and fixed img alt warning

--- src/components/Encode.js
+++ src/components/Encode.js
@@ -1,15 +1,18 @@
 import Input from "./elements/Input"
 import Textarea from "./elements/Textarea"
 import Button from "./elements/Button"
+import { useState } from "react"
 
 const Encode = ({ imageData, loadImage }) => {
 
+    const [ message, setMessage ] = useState(null)
+
     return (
         <div id='encode'>
-            <div class="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4">
                 <div>
-                    { imageData && <img alt='Uploaded image' src={ imageData } />}
-                    { imageData && <Textarea />}
+                    { imageData && <img alt='Message carrier' src={ imageData } />}
+                    { imageData && <Textarea placeholder='Enter your message to be encoded' onChange={ setMessage } value={ message } />}
                     <Input loadImage={ loadImage } type='file' />
                 </div>
                 <div>
