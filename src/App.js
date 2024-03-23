cae228b935b24b479d2433057950aaf7e3cccc02
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-23 23:08:29 +0100

    deleted unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Tabs from './components/Tabs';
 import Encode from './components/Encode';
